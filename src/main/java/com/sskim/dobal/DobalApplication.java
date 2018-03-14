767ae70d9204cf81091ace59ac8b9912c6a0e4a3
sskim <tjdtjq91@gmail.com>
2018-03-14 18:15:40 +0900

    startbootstrap dashboard theme 붙여보기

---
+++ src/main/java/com/sskim/dobal/DobalApplication.java
@@ -0,0 +1,12 @@
+package com.sskim.dobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobalApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sskim/dobal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sskim.dobal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DobalApplication.class);
+	}
+
+}
